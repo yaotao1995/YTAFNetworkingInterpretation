1d88d6aea8e34082713408dae1c98e4d3f1f7766
july_yt <1007881775@qq.com>
2019-03-27 15:54:38 +0800

    第一次上传

---
+++ Pods/Target Support Files/Pods-YTAFNetworkingInterpretation/Pods-YTAFNetworkingInterpretation-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YTAFNetworkingInterpretation : NSObject
+@end
+@implementation PodsDummy_Pods_YTAFNetworkingInterpretation
+@end
---
+++ Pods/Target Support Files/Pods-YTAFNetworkingInterpretationUITests/Pods-YTAFNetworkingInterpretationUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YTAFNetworkingInterpretationUITests : NSObject
+@end
+@implementation PodsDummy_Pods_YTAFNetworkingInterpretationUITests
+@end
